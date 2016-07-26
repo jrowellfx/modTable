54bd797c44b2459c92333b3f3e7b1528b038c4aa
James Rowell <jrowell@discreet.cgtown>
2016-07-25 23:09:34 -0500

    Initial commit - fully working fun toy program

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python2.7
+
+from distutils.core import setup
+import os
+import shutil
+
+setup(name='modTable',
+      version='1.000',
+      description='fun toy program to print addition and multiplication tables for Z mod m*Z',
+      long_description='',
+      author='James Philip Rowell',
+      author_email='james@oic-inc.net',
+      url='http://www.orangeimagination.com/',
+      py_modules=[],
+      scripts=['modTable'],
+      license = "BSD 3-Clause license",
+     )
